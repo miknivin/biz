1efc4835819b01271923aa3127b5042fd3b684c5
miknivin <116654570+miknivin@users.noreply.github.com>
2025-04-06 00:39:40 +0530

    new-8

--- app/layout.js
+++ app/layout.js
@@ -3,13 +3,19 @@ import "../public/assets/css/bootstrap.css";
 import "../public/assets/css/color.css";
 import "../public/assets/css/style.css";
 import "swiper/css";
-// import "swiper/css/navigation"
 import "swiper/css/pagination";
 import "swiper/css/free-mode";
 import { poppins } from "@/lib/font";
+
 export const metadata = {
-  title: "Biz incorp",
-  description: "Biz incorp",
+  title: "Biz Incorp",
+  description: "We help businesses grow.",
+  openGraph: {
+    title: "Biz Incorp",
+    description: "We help businesses grow.",
+    url: "https://www.thebizincorp.com/",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
