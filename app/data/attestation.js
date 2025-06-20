82c95a46908727ad4bc27d9f36e3b26e0fcb03ae
miknivin <116654570+miknivin@users.noreply.github.com>
2025-06-20 20:02:25 +0530

    country 404 resolved

--- app/data/attestation.js
+++ app/data/attestation.js
@@ -57,7 +57,7 @@ export const galleryItems = [
     id: 6,
     image: "/assets/images/flags/sweden flag.jpg",
     categories: ["cat-1"],
-    country: "sweden",
+    country: "Sweden",
     title: "Sweden",
     description: "Setup your LLC or Corporation easily.",
     content:
@@ -67,7 +67,7 @@ export const galleryItems = [
     id: 7,
     image: "/assets/images/flags/switzerland flag.jpg",
     categories: ["cat-1"],
-    country: "switzerland",
+    country: "Switzerland",
     title: "Switzerland",
     description: "Setup your LLC or Corporation easily.",
     content:
@@ -97,7 +97,7 @@ export const galleryItems = [
     id: 10,
     image: "/assets/images/flags/qatar.jpeg",
     categories: ["cat-1"],
-    country: "qatar",
+    country: "Qatar",
     title: "Qatar",
     description: "Setup your LLC or Corporation easily.",
     content:
